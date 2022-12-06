2ab32c98c0395266eb2eef83a8a6c20938379d4d
ariet001 <kalmamatovariet43@gmail.com>
2022-12-06 21:10:03 +0600

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,10 @@
+public class Circle {
+    private double PI;
+    public void name(double PI){
+        double radius = PI;
+        double name = Math.PI*(radius*radius);
+        System.out.println("площадь окружнение:"+name );
+        double circumference = Math.PI*2*radius;
+        System.out.println("длина окружности равно:"+circumference);
+    }
+}
